f8fca762bc6fd6fdc144a7240a1c00d26edab2b1
Jack <yy4r@github.com>
2020-04-14 16:21:14 +0800

    tijiao

---
+++ jack/src/main/java/com/example/jack/JackApplication.java
@@ -0,0 +1,13 @@
+package com.example.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackApplication.class, args);
+    }
+
+}
---
+++ jack/src/test/java/com/example/jack/JackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
